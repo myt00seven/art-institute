72dc303a45621a062aa2d89101a9ea2e0fcfe1fc
myt00seven <>
2019-06-06 03:07:48 -0500

    testing

--- ArtByRange.py
+++ ArtByRange.py
@@ -4,7 +4,15 @@ import os
 import urllib
 import re
 
+MAX_FILE_DOWNLOAD_COUNT = 1
+
+file_downloaded_count = 0
+
 for work in range(93000, 92800, -1):
+
+    if file_downloaded_count >= MAX_FILE_DOWNLOAD_COUNT:
+        break
+
     urlbase = 'https://www.artic.edu/artworks/'
     url = urlbase + str(work)
     print(url)
@@ -14,6 +22,8 @@ for work in range(93000, 92800, -1):
         res.raise_for_status()
 
         soup = bs4.BeautifulSoup(res.text, 'lxml')
+
+        print(soup)
 
         item = soup.find('div', class_='m-article-header__img-container')
 
@@ -25,48 +35,50 @@ for work in range(93000, 92800, -1):
 
             if cleanlic == "CC0 Public Domain Designation":
         
-            #title
-            titletag = soup.find('h1', class_='sr-only')
-            title = titletag.text
-            print(title)
+                #title
+                titletag = soup.find('h1', class_='sr-only')
+                title = titletag.text
+                print(title)
 
-            #artist
-            #TODO come back and do this right
-            #artist = soup.find("data-gtm-event")
-            #print(artist)    
+                #artist
+                #TODO come back and do this right
+                #artist = soup.find("data-gtm-event")
+                #print(artist)    
 
-            #set or create a folder based on art style
-            mydirectory = soup.find(itemprop="provider").get("content")
-            os.makedirs(mydirectory, exist_ok=True)
-            print(mydirectory)
+                #set or create a folder based on art style
+                mydirectory = soup.find(itemprop="provider").get("content")
+                os.makedirs(mydirectory, exist_ok=True)
+                print(mydirectory)
 
-            try: 
-                item.img.get("data-iiifid")
-                imglink = item.img.get("data-iiifid")
-                fulllink = imglink + '/full/4000,/0/default.jpg'
+                try: 
+                    item.img.get("data-iiifid")
+                    imglink = item.img.get("data-iiifid")
+                    fulllink = imglink + '/full/4000,/0/default.jpg'
 
-                #rename the file with the title and artist
-                filename = title + ' -- ' + str(work) + '.jpg'
-                #filename = title + '.jpg'
-                print(filename)
+                    #rename the file with the title and artist
+                    filename = title + ' -- ' + str(work) + '.jpg'
+                    #filename = title + '.jpg'
+                    print(filename)
 
-                #download the file
-                print('Downloading image %s...' % (fulllink))
-                res = requests.get(fulllink)
-                res.raise_for_status()
+                    #download the file
+                    print('Downloading image %s...' % (fulllink))
+                    res = requests.get(fulllink)
+                    res.raise_for_status()
 
-                #save the file
-                imageFile = open(os.path.join(mydirectory, os.path.basename(filename)), 'wb')
-                for chunk in res.iter_content(1000000):
-                    imageFile.write(chunk)
-                imageFile.close()
+                    #save the file
+                    imageFile = open(os.path.join(mydirectory, os.path.basename(filename)), 'wb')
+                    for chunk in res.iter_content(1000000):
+                        imageFile.write(chunk)
+                    imageFile.close()
 
-                with open("log.txt", "a") as f:
-                    f.write(str(work) + "|" + title +   "\n")
+                    with open("log.txt", "a") as f:
+                        f.write(str(work) + "|" + title +   "\n")
 
-            except:
-                with open("log.txt", "a") as f:
-                    f.write(str(work) + "| IMAGE UNAVAILABLE \n")
+                    file_downloaded_count += 1
+
+                except:
+                    with open("log.txt", "a") as f:
+                        f.write(str(work) + "| IMAGE UNAVAILABLE \n")
     except:
         with open("log.txt", "a") as f:
                 f.write(str(work) + "| URL UNAVAILABLE \n")
